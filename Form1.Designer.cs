9df2ed7454ae11883f6435c2d70fcaac8411edaa
Simon Anthony <simon.anthony@tortysoft.com>
2020-05-04 20:56:11 +0100

    Working - need divide fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WindowsFormsApp2
+﻿using System;
+
+namespace WindowsFormsApp2
 {
     partial class Form1
     {
@@ -40,6 +42,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -103,10 +111,54 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // listBox1
+            // 
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            resources.GetString("listBox1.Items"),
+            resources.GetString("listBox1.Items1"),
+            resources.GetString("listBox1.Items2"),
+            resources.GetString("listBox1.Items3"),
+            resources.GetString("listBox1.Items4")});
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
@@ -137,6 +189,13 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        //private EventHandler listBox1_SelectedIndexChanged;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,10 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        int Number;
+        string text;
+        string text2;
+        string test;
 
         public Form1()
         {
@@ -32,30 +36,97 @@ namespace WindowsFormsApp2
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             CheckNumbers(textBox1.Text, textBox2.Text);
+            DoMath(text, text2);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             CheckNumbers(textBox2.Text, textBox1.Text);
+            DoMath(text2,text);
         }
 
         public int CheckNumbers(string text, string text2)
         {
-            int Number;
-            Number = 0;
+            label9.Text = text;
+            label10.Text = text2;
+
+            // Number = 0;
             if ((text != null) && (text2 != null))
-            { 
-                bool success =(int.TryParse(text, out Number))&& (int.TryParse(text2, out Number));
+            {
+                bool success = (int.TryParse(text, out Number)) && (int.TryParse(text2, out Number));
                 if (!success)
                 {
-                 
-                   outputLbl.Text = "Not a number";
-                   return Number;
+
+                    outputLbl.Text = "Not a number";
+                    return Number;
                 }
+                DoMath(text, text2);
+                DoMath(text2, text);
             }
-            Number = int.Parse(text) + int.Parse(text2);
-            outputLbl.Text = Number.ToString();
+      
             return Number;
         }
+
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           
+
+             test = listBox1.GetItemText(listBox1.SelectedItem);
+            CheckNumbers(textBox1.Text, textBox2.Text);
+        }
+        public void DoMath(string text, string text2)
+        {
+
+            //label6.Text = "GOTIT";
+           // label6.Text = "switch test before= " + test;
+          if ((text != null) && (text2 != null))
+            {
+
+               // label8.Text = "switch test after= "+test;
+                switch (test)
+                {
+                    case "Plus":
+                        Number = int.Parse(text) + int.Parse(text2);
+                        outputLbl.Text = Number.ToString();
+                   // label7.Text = "output " + Number.ToString();
+
+                    break;
+                    case "Minus":
+                        Number = int.Parse(text) - int.Parse(text2);
+                        outputLbl.Text = Number.ToString();
+                    //label7.Text = "output " + Number.ToString();
+                    break;
+                    case "Multiply":
+                        Number = int.Parse(text) * int.Parse(text2);
+                        outputLbl.Text = Number.ToString();
+                    //label7.Text = "output " + Number.ToString();
+                    break;
+                    case "Divide":
+                        Number = int.Parse(text) / int.Parse(text2);
+                        outputLbl.Text = Number.ToString();
+                   // label7.Text = "output " + Number.ToString();
+                    break;
+                    case "Power":
+                        Number = int.Parse(text) * int.Parse(text2) * int.Parse(text2);
+                        outputLbl.Text = Number.ToString();
+                  
+                        break;
+                    default:
+                        //label6.Text = "working on null";
+                        //Number = 2000;
+                    //label7.Text = "output " + Number.ToString();
+                    break;
+                }
+            }
+             label7.Text = "output "+Number.ToString(); 
+           // outputLbl.Text = Number.ToString();
+            //label8.Text = "116 " + Number.ToString();
+
+        }
     }
+           
+
+
 }
+
